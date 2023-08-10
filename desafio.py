5ed4d4e4d3b64aa4cbbf338906da6bcf89edc6a6
LucasPires50 <luc_50@outlook.com>
2023-08-10 10:58:48 -0300

    feat: criação de usuário

--- desafio.py
+++ desafio.py
@@ -1,4 +1,13 @@
 from operacoes import formatar_float_para_real
+from usuario import criar_usuario
+
+login = """
+    [1] Fazer login
+    [2] Criar conta
+
+Escolha um opção:
+
+"""
 
 operacoes = """
 
@@ -16,53 +25,63 @@ limite_diario_de_saque = 500
 extrato = []
 numeros_de_saques = 0
 limete_de_saques_diario = 3
+banco_de_usuario = []
+banco_de_contas = []
+logado = ""
 
 while True:
-    opcao = input(operacoes)
+    if not logado:
+        opcao = input(login)
+        if opcao == '1':
+            print("fazer login")
+        elif opcao == '2':
+            logado = criar_usuario(banco_de_usuario)
+    else:
+        opcao = input(operacoes)
     
-    if opcao == '1':
-        print("Depósito\n")
-        valor_deposito = input("Informe o valor do depósito:")
-        
-        if valor_deposito.replace(".", "", 1).isdigit():
-            valor_deposito = float(valor_deposito)
-            saldo += valor_deposito
-            extrato.append({"tipo": "depósito", "valor": valor_deposito})
+        if opcao == '1':
+            print("Depósito\n")
+            valor_deposito = input("Informe o valor do depósito:")
             
-        else:
-            print("Valor Inválido!")
-            
-    elif opcao == '2':
-        print("Saque\n")
-        valor_saque = input("Informe o valor a ser sacado:")
-        
-        if valor_saque.replace(".", "", 1).isdigit():
-            valor_saque = float(valor_saque)
-            if valor_saque > saldo:
-                print("Saldo Insuficiente!")
-            elif numeros_de_saques == limete_de_saques_diario:
-                print("Limite de saque diário atingido!")
-            else:
-                valor_total_sacado += valor_saque
-                if valor_total_sacado > limite_diario_de_saque:
-                    print("Limite de valor de saque diário atingido!")
-                saldo -= valor_saque
-                numeros_de_saques += 1
-                extrato.append({"tipo": "saque", "valor": valor_saque})
-        else:
-            print("Valor Inválido!")
-        
-    elif opcao == '3':
-        titulo = " Extrato "
-        print(len(titulo))
-        print(titulo.center(20, "#"))
-        if extrato:
-            for trasacao in extrato:
-                print(f'{trasacao["tipo"]} : R$ {formatar_float_para_real(trasacao["valor"])}')
+            if valor_deposito.replace(".", "", 1).isdigit():
+                valor_deposito = float(valor_deposito)
+                saldo += valor_deposito
+                extrato.append({"tipo": "depósito", "valor": valor_deposito})
                 
-            print(f"\nSaldo: R$ {formatar_float_para_real(saldo)}")
-        else:
-            print("Não foram realizadas movimentações.")
-        print("".center(10+len(titulo), "#"))
-    elif opcao == 'x':
-        break
+            else:
+                print("Valor Inválido!")
+                
+        elif opcao == '2':
+            print("Saque\n")
+            valor_saque = input("Informe o valor a ser sacado:")
+            
+            if valor_saque.replace(".", "", 1).isdigit():
+                valor_saque = float(valor_saque)
+                if valor_saque > saldo:
+                    print("Saldo Insuficiente!")
+                elif numeros_de_saques == limete_de_saques_diario:
+                    print("Limite de saque diário atingido!")
+                else:
+                    valor_total_sacado += valor_saque
+                    if valor_total_sacado > limite_diario_de_saque:
+                        print("Limite de valor de saque diário atingido!")
+                    saldo -= valor_saque
+                    numeros_de_saques += 1
+                    extrato.append({"tipo": "saque", "valor": valor_saque})
+            else:
+                print("Valor Inválido!")
+            
+        elif opcao == '3':
+            titulo = " Extrato "
+            print(len(titulo))
+            print(titulo.center(20, "#"))
+            if extrato:
+                for trasacao in extrato:
+                    print(f'{trasacao["tipo"]} : R$ {formatar_float_para_real(trasacao["valor"])}')
+                    
+                print(f"\nSaldo: R$ {formatar_float_para_real(saldo)}")
+            else:
+                print("Não foram realizadas movimentações.")
+            print("".center(10+len(titulo), "#"))
+        elif opcao == 'x':
+            break
---
+++ usuario.py
@@ -0,0 +1,35 @@
+
+def criar_usuario(banco_de_usuario):
+    
+    print(f"\nPreencha as informações abaixo: \n")
+    nome = input("Nome: ")
+    data_nascimento = input("Data de nascimento: ")
+    cpf = input("cpf: ").replace(".", "").replace("-", "")
+    logadouro = input("logadouro: ")
+    numero = input("numero: ")
+    bairro = input("bairro: ")
+    cidade = input("cidade: ")
+    estado = input("estado: ")
+    
+    validar_estado(estado)
+    verifica_cpf_utilizado(banco_de_usuario, cpf)
+    
+    banco_de_usuario.append({
+        "nome": nome,
+        "data_nascimento": data_nascimento,
+        "cpf": cpf,
+        "endereco": f"{logadouro} - {numero} - {bairro} - {cidade}/{estado}",
+    })
+    
+    return cpf
+    
+def validar_estado(estado):
+    if not len(estado) == 2:
+        print("Preencha somente com a sigla do estado.")
+
+def verifica_cpf_utilizado(banco_de_usuario, cpf):
+    for usuario in banco_de_usuario:
+        usuario_cpf = usuario.get("cpf", None)
+        print(usuario_cpf)
+        if cpf == usuario_cpf:
+            print("O cpf já foi utilizado.")
